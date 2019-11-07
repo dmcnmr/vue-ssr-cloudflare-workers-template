a4f88b524e9f9627cb7dabee59def9a46534a5ea
l5x <189198+l5x@users.noreply.github.com>
2019-11-07 04:11:55 +0400

    chore: delete commented out code

--- src/entry-worker.js
+++ src/entry-worker.js
@@ -5,10 +5,9 @@ import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 
 /**
  * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
+ * 1. Skip caching on the edge, which makes it easier to
  *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
+ * 2. Return an error message on exception in your Response.
  */
 const DEBUG = false
 
@@ -74,15 +73,8 @@ addEventListener('fetch', event => {
 })
 
 async function handleEvent(event) {
-  // const url = new URL(event.request.url)
-
   let options = {}
 
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
   if (DEBUG) {
     // customize caching
     options.cacheControl = {
@@ -93,22 +85,6 @@ async function handleEvent(event) {
   try {
     return await getAssetFromKV(event, options)
   } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    // if (!DEBUG) {
-    //   try {
-    //     let notFoundResponse = await getAssetFromKV(event, {
-    //       mapRequestToAsset: req =>
-    //         new Request(`${new URL(req.url).origin}/404.html`, req)
-    //     })
-    //
-    //     return new Response(notFoundResponse.body, {
-    //       ...notFoundResponse,
-    //       status: 404
-    //     })
-    //     // eslint-disable-next-line no-empty
-    //   } catch (e) {}
-    // }
-
     const url = new URL(event.request.url)
 
     const init = {
